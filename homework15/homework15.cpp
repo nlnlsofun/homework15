1006d0b274cb551d592f7a27602b71e531d340bc
柯仁傑 <@DESKTOP-34DDU9R>
2023-04-16 16:19:24 +0800

    加入專案檔案。

---
+++ homework15/homework15.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <cctype>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);  // 讀入一行文字
+    int word_count = 0;
+    unordered_map<char, int> char_count;
+    for (int i = 0; i < s.length(); i++) {
+        if (isalpha(s[i])) {
+            char_count[tolower(s[i])]++;
+        }
+        else if (s[i] == ' ' || s[i] == ',' || s[i] == '.') {
+            if (i > 0 && isalpha(s[i - 1])) {
+                word_count++;
+            }
+        }
+    }
+    // 處理最後一個字
+    if (isalpha(s[s.length() - 1])) {
+        word_count++;
+    }
+
+    cout << word_count << endl;
+    for (char c = 'a'; c <= 'z'; c++) {
+        if (char_count.count(c) > 0) {
+            cout << c << " " << char_count[c] << endl;
+        }
+    }
+    return 0;
+}
